ca837235502509f8a20a7353131b3113555b3a73
Aleksandr Pak <aleksandr.pak@mendix.com>
2016-03-02 20:20:45 +0100

    first version of insertion sort

--- src/sort/insertion.rs
+++ src/sort/insertion.rs
@@ -1,11 +1,18 @@
-use std::iter::Iterator;
-
-pub fn sort<T: Sized>(iter: &mut Iterator<Item = T>) -> Vec<T> {
-    let mut result = vec![];
-
-    for item in iter {
-
+pub fn sort<T: PartialOrd>(arr: &mut [T]) {
+    for i in 1..arr.len() {
+        let mut j = i;
+        while j > 0 && arr[j - 1] > arr[j] {
+            arr.swap(j - 1, j);
+            j = j - 1;
+        }
     }
+}
 
-    result
+#[test]
+fn test_insertion_sort_simple() {
+    let mut arr = [-5, 4, 1, -3, 2];
+
+    sort(&mut arr);
+
+    assert!(arr == [-5, -3, 1, 2, 4]);
 }
