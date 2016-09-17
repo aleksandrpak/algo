c0c585249768e5e657959e767f3b48551fd97b9b
Austin Bonander <austin.bonander@gmail.com>
2016-09-16 19:08:09 -0700

    Add variants of qselect functions which take ordering functions

--- src/select/qselect.rs
+++ src/select/qselect.rs
@@ -1,5 +1,7 @@
+use std::cmp::Ordering;
 use std::mem;
 
+// The threshold below which qselect_inplace_by() should just sort the slice.
 const SORT_THRESH: usize = 6;
 
 /// Use the Quickselect algorithm to select the `k`th smallest
@@ -11,11 +13,23 @@ const SORT_THRESH: usize = 6;
 /// ##Panics
 /// If `k` is greater than `data.len()`.
 pub fn qselect<T: Ord>(data: &[T], k: usize) -> &T {
+    qselect_by(data, k, <T as Ord>::cmp)
+}
+
+/// Use the Quickselect algorithm, given an ordering function, to select the `k`th
+/// smallest element from `data`.
+///
+/// Does not modify `data`. Allocates an additional vector for scratch space; see `qselect_inplace_by`
+/// for an in-place version.
+///
+/// ##Panics
+/// If `k` is greater than `data.len()`.
+pub fn qselect_by<T, F: FnMut(&T, &T) -> Ordering>(data: &[T], k: usize, mut ord_fn: F) -> &T {
     let len = data.len();
     assert!(k < len, "Called qselect with k = {} and data length: {}", k, len);
 
     let mut refs: Vec<_> = data.iter().collect();
-    *qselect_inplace(&mut refs, k)
+    *qselect_inplace_by(&mut refs, k, |l, r| ord_fn(l, r))
 }
 
 /// Use the Quickselect algorithm to select the `k`th smallest element from `data`. As part of the
@@ -27,27 +41,41 @@ pub fn qselect<T: Ord>(data: &[T], k: usize) -> &T {
 /// ##Panics
 /// If `k` is greater than `data.len()`.
 pub fn qselect_inplace<T: Ord>(data: &mut [T], k: usize) -> &mut T {
+    qselect_inplace_by(data, k, <T as Ord>::cmp)
+}
+
+/// Use the Quickselect algorithm, given an ordering function,
+/// to select the `k`th smallest element from `data`. As part of the
+/// algorithm, `k` is moved to its final sorted position and the rest of the array is (at least) partially
+/// sorted.
+///
+/// For a version that uses extra space but does not reorder the slice, use `qselect_by`.
+///
+/// ##Panics
+/// If `k` is greater than `data.len()`.
+pub fn qselect_inplace_by<T, F: FnMut(&T, &T) -> Ordering>(data: &mut [T], k: usize, mut ord_fn: F) -> &mut T {
     let len = data.len();
 
     assert!(k < len, "Called qselect_inplace with k = {} and data length: {}", k, len);
 
-    if len < SORT_THRESH { 
-        data.sort();  
+    if len < SORT_THRESH {
+        data.sort_by(&mut ord_fn);
         return &mut data[k];
     }
 
-    let pivot_idx = partition(data);
+    let pivot_idx = partition_by(data, &mut ord_fn);
 
     if k == pivot_idx {
         &mut data[pivot_idx]
     } else if k < pivot_idx {
-        qselect_inplace(&mut data[..pivot_idx], k)
+        qselect_inplace_by(&mut data[..pivot_idx], k, ord_fn)
     } else {
-        qselect_inplace(&mut data[pivot_idx + 1..], k - pivot_idx - 1)
+        qselect_inplace_by(&mut data[pivot_idx + 1..], k - pivot_idx - 1, ord_fn)
     }
 }
 
-fn partition<T: Ord>(data: &mut [T]) -> usize {
+/// Given an ordering function, pick an arbitrary pivot and partition the slice, returning the pivot.
+fn partition_by<T, F: FnMut(&T, &T) -> Ordering>(data: &mut [T], mut ord_fn: F) -> usize {
     let len = data.len();
 
     let pivot_idx = {
@@ -55,7 +83,7 @@ fn partition<T: Ord>(data: &mut [T]) -> usize {
         let mid = (&data[len / 2], len / 2);
         let last = (&data[len - 1], len - 1);
 
-        median_of_3(&first, &mid, &last).1
+        median_of_3_by(&first, &mid, &last, |left, right| ord_fn(left.0, right.0)).1
     };
 
     data.swap(pivot_idx, len - 1);
@@ -63,7 +91,7 @@ fn partition<T: Ord>(data: &mut [T]) -> usize {
     let mut curr = 0;
 
     for i in 0 .. len - 1 {
-        if data[i] < data[len - 1] {
+        if ord_fn(&data[i], &data[len - 1]) == Ordering::Less {
             data.swap(i, curr);
             curr += 1;
         }
@@ -74,13 +102,17 @@ fn partition<T: Ord>(data: &mut [T]) -> usize {
     curr
 }
 
-
 /// Of the three values passed, return the median.
-pub fn median_of_3<T: Ord>(mut x: T, mut y: T, mut z: T) -> T {
-    in_order(&mut x, &mut y);
-    in_order(&mut x, &mut z);
-    in_order(&mut y, &mut z);
-    
+pub fn median_of_3<T: Ord>(x: T, y: T, z: T) -> T {
+    median_of_3_by(x, y, z, <T as Ord>::cmp)
+}
+
+/// Given an ordering function, of the three values passed, return the median.
+pub fn median_of_3_by<T, F: FnMut(&T, &T) -> Ordering>(mut x: T, mut y: T, mut z: T, mut ord_fn: F) -> T {
+    in_order_by(&mut x, &mut y, &mut ord_fn);
+    in_order_by(&mut x, &mut z, &mut ord_fn);
+    in_order_by(&mut y, &mut z, &mut ord_fn);
+
     y
 }
 
@@ -88,6 +120,14 @@ pub fn median_of_3<T: Ord>(mut x: T, mut y: T, mut z: T) -> T {
 #[inline]
 pub fn in_order<T: Ord>(x: &mut T, y: &mut T) {
     if x > y {
+        mem::swap(x, y);
+    }
+}
+
+/// Given an ordering function, if `x > y`, swap `x` and `y`.
+#[inline]
+pub fn in_order_by<T, F: FnMut(&T, &T) -> Ordering>(x: &mut T, y: &mut T, mut ord_fn: F) {
+    if ord_fn(x, y) == Ordering::Greater {
         mem::swap(x, y);
     }
 }
